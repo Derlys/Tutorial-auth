3b585125467cb3192da18e3f74f0dc4986a88fd8
Derlys Dominguez <derlys275@gmail.com>
2020-10-07 16:37:37 -0500

    utilizamos nuestro servicio en el componente

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor(private router: Router, private service: AuthService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  loginGoogle(): void {
+    this.service.loginGoogle().subscribe((res) => {
+      console.log(res);
+      return this.router.navigate(['/']);
+    });
   }
-
 }
