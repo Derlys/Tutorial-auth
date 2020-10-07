e9fed6325556bac2139ea52bd3f31e846f235e2d
Derlys Dominguez <derlys275@gmail.com>
2020-10-07 14:00:14 -0500

    introducción Kikstart UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { UiLink } from '@kikstart-ui/ui-link';
+import { UiBrand } from '@kikstart-ui/ui-brand';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tutorial-auth';
+  brand: UiBrand = {
+    name: 'Site',
+    logo: 'https://github.com/kikstart-ui.png',
+  };
+  links: UiLink[] = [
+    { label: 'Home', path: '/' },
+    { label: 'About', path: '/about' },
+    { label: 'Contact', path: '/contact' },
+  ];
 }
