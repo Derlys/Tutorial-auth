be783e88c23a652b509d2cb33ad3fc239ee2e940
Derlys Dominguez <derlys275@gmail.com>
2020-10-07 16:52:59 -0500

    projecto firebase para usar autenticación

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDd6YAt4yCsWFA1XQWiyVRDPJ0A9JOwMW4',
+    authDomain: 'site2-8ce5b.firebaseapp.com',
+    databaseURL: 'https://site2-8ce5b.firebaseio.com',
+    projectId: 'site2-8ce5b',
+    storageBucket: 'site2-8ce5b.appspot.com',
+    messagingSenderId: '586449426776',
+    appId: '1:586449426776:web:08a1b860739a1c5ec26d2d',
+    measurementId: 'G-QTB8DML5KJ',
+  },
 };
 
 /*
